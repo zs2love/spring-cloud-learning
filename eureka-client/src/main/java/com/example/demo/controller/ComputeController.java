7244e875aad8bdee0ed95d11e4b1a594d6b6de40
zs2love <zs2love@126.com>
2017-09-12 19:08:35 +0800

    Modify the compute controller.

--- eureka-client/src/main/java/com/example/demo/controller/ComputeController.java
+++ eureka-client/src/main/java/com/example/demo/controller/ComputeController.java
@@ -7,7 +7,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
